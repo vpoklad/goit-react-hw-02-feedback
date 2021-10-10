c2bf1444940c7c5cd732afc1b4beabaadd4e119b
Viktor Poklad <3d.poklad@gmail.com>
2021-10-10 21:02:23 +0300

    добавляет начальный функционал

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+
+    return Math.round((this.state.good * 100) / total);
+  };
+  onLeaveFeedback = e => {
+    const name = e.target.name;
+    this.setState(prev => ({ [name]: prev[name] + 1 }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const posPercentage = this.countPositiveFeedbackPercentage();
+    return (
+      <article>
+        <h2>Please leave feaadback</h2>
+        <button type="button" name="good" onClick={this.onLeaveFeedback}>
+          Good
+        </button>
+        <button type="button" name="neutral" onClick={this.onLeaveFeedback}>
+          Neutral
+        </button>
+        <button type="button" name="bad" onClick={this.onLeaveFeedback}>
+          Bad
+        </button>
+        <h3>Satistics</h3>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Positive feadback: {posPercentage}%</p>
+      </article>
+    );
+  }
 }
 
 export default App;
